f4e6cd1b5505ddfd08737b649b313118d721e71d
Saptarshi Dey <dey7.kol@gmail.com>
2017-12-02 23:08:07 +0530

    set up a sass compiler in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,18 @@ var path = require('path')
 
 var DIST_DIR=path.resolve(__dirname,'dist');
 var SRC_DIR=path.resolve(__dirname,'src');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 var config={
-    entry: SRC_DIR + "/app/index.js",
+    entry: [SRC_DIR + "/app/index.js",SRC_DIR + '/scss/main.scss'],
     output:{
         path: DIST_DIR+"/app",
-        fileName:"bundle.js",
+        filename:"bundle.js",
         publicPath:"/app/"
     },
     module:{
-        loader:[
+        rules:[
             {
                 test:/\.js?/,
                 include: SRC_DIR,
@@ -20,9 +22,27 @@ var config={
                 query:{
                     presets:["react","es2015","stage-2"]
                 }
-            }
+            },
+            { // regular css files
+                test: /\.css$/,
+                include:SRC_DIR,
+                loader: ExtractTextPlugin.extract({
+                  loader: 'css-loader?importLoaders=1',
+                }),
+            },
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                include:SRC_DIR,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }        
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({ // define where to save the file
+          filename: 'dist/scss/styles.bundle.css',
+          allChunks: true,
+        }),
+      ],   
 }
 
 module.exports=config;
