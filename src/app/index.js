5e33c3c3f47391731360820fd21dc9a632a6bf21
Saptarshi Dey <dey7.kol@gmail.com>
2017-12-05 22:38:56 +0530

    navbar component added

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM,{render} from "react-dom";
-
+import Navbar from "./components/Navbar"
 class App extends React.Component{
     render(){
         return (
             <div>
-                <h1>Welcome!</h1>
+                <Navbar />
             </div>
         )
     }
