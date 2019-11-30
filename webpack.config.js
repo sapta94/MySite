f7d17b3f48859196fbbc3b5bc34cc103c2b845ab
sapta94 <dey7.kol@gmail.com>
2019-11-30 21:26:13 +0530

    css loader in config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ var config={
                 query:{
                     presets:["react","es2015","stage-2"]
                 }
-            }     
+            },
+            { test: /\.css$/, use: 'css-loader' }    
         ]
     }
 }
