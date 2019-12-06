009de1ed4d7367eac9a1994d439dbbd876693a15
Saptarshi <dey7.kol@gmail.com>
2019-12-06 16:51:16 +0530

    nav contents center

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -15,12 +15,12 @@ class Navigation extends React.Component{
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="mr-auto pull-center">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#career">Career</Nav.Link>
                         <Nav.Link href="#projects">Projects</Nav.Link>
-                        <Nav.Link href="#conatct">Contact</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
