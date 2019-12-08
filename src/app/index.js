54fe399bd97dd9e2455e7291d6d38c735c22e94f
sapta94 <dey7.kol@gmail.com>
2019-12-08 19:41:45 +0530

    style chnages for navbar and new deployment scripts

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM,{render} from "react-dom";
 import Navigation from "./components/Navbar"
+import Timeline from "./components/Timeline"
 import "../css/main.css"
+import 'react-vertical-timeline-component/style.min.css';
+import { log } from "util";
 class App extends React.Component{
+   
     render(){
         return (
             <div>
-                <Navigation />
+                <Navigation/>
+                <Timeline />
             </div>
         )
     }
