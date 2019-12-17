b6bf80a89ee936ce89e367421df9cf925b9a1b50
Saptarshi <dey7.kol@gmail.com>
2019-12-17 10:11:19 +0530

    adding linkedin card

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -18,6 +18,9 @@ export default class Contact extends Component {
                     <i style={{margin:'0 5% 0 0'}} className="fa fa-phone fa-4x" aria-hidden="true"></i>
                     <span className='creds'>(+91)-8981235949</span>
                 </div>
+                <div className='col-md-4'>
+                  <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="dark" data-vanity="saptarshi-dey-97b313bb"><a class="LI-simple-link" href='https://in.linkedin.com/in/saptarshi-dey-97b313bb?trk=profile-badge'>Saptarshi Dey</a></div>
+                </div>
             </div>
         </div>
        </div>
