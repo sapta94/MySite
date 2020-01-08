6b22adaf3c0a8d7e50633fe343551a8c9cdf916f
sapta94 <dey7.kol@gmail.com>
2020-01-08 23:36:32 +0530

    chnages for adding counter

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -10,13 +10,19 @@ export default class About extends Component {
       ind:null
     }
   }
-
-  componentDidUpdate(nextProps){
-    if(nextProps.ind!=this.props.ind){
-      this.setState({
-        ind:nextProps.ind
-      })
-    }
+  componentDidMount(){
+    var that=this
+    window.addEventListener("scroll",  function(event){
+        let scroll = this.scrollY;
+        let homDiv = document.getElementById("home").scrollHeight;
+        let aboutDiv = document.getElementById("about").scrollHeight
+        
+        if(scroll>=homDiv-23 && scroll<homDiv+aboutDiv-65){
+          that.setState({
+            ind:2
+          })
+      }
+    })
   }
   render() {
     return (
@@ -28,17 +34,84 @@ export default class About extends Component {
             <div className='container-fluid'>
               <div className='row'>
                 <div className='col-md-4 side-image'>
-                {
-                  this.state.ind==2?<CountUp start={0} end={100} delay={0}>
-                  {({ countUpRef }) => (
-                    <div>
-                      <span ref={countUpRef} />
+                  <div className='row'>
+                    <div className='col-md-6'>
+                        {
+                        this.state.ind==2?<CountUp start={5000} end={100000} delay={0}>
+                        {({ countUpRef }) => (
+                          <div>
+                            <span ref={countUpRef} />
+                          </div>
+                        )}
+                      </CountUp>:null
+              
+                      }
+                      <span className='labels'>Lines of Code</span>
                     </div>
-                  )}
-                </CountUp>:null
-                }
+         
+                    <div className='col-md-6'>
+                    {
+                        this.state.ind==2?<CountUp start={0} end={100} delay={0}>
+                        {({ countUpRef }) => (
+                          <div>
+                            <span ref={countUpRef} />
+                          </div>
+                        )}
+                      </CountUp>:null
+              
+                      }
+                      <span  className='labels'>Dedication</span>
+                    </div>
+
+                  </div>
+                  <br/>
+                  <div className='row'>
+                    <div className='col-md-6'>
+                        {
+                        this.state.ind==2?<CountUp start={0} end={100} delay={0}>
+                        {({ countUpRef }) => (
+                          <div>
+                            <span ref={countUpRef} />
+                          </div>
+                        )}
+                      </CountUp>:null
+              
+                      }
+                      <span  className='labels'>Persistance</span>
+                    </div>
+                    <div className='col-md-6'>
+                    {
+                        this.state.ind==2?<CountUp start={0} end={80} delay={0}>
+                        {({ countUpRef }) => (
+                          <div>
+                            <span ref={countUpRef} />
+                          </div>
+                        )}
+                      </CountUp>:null
+              
+                      }
+                      <span  className='labels'>Cups of Beverage</span>
+                    </div>
+                  </div>
+                  <br/>
+                  <div className='row'>
+                    <div className='col-md-6'>
+                        {
+                        this.state.ind==2?<CountUp start={0} end={60} delay={0}>
+                        {({ countUpRef }) => (
+                          <div>
+                            <span ref={countUpRef} />
+                          </div>
+                        )}
+                      </CountUp>:null
+              
+                      }
+                      <span  className='labels'>Geek</span>
+                    </div>
+                  </div>
+
                 </div>
-                <div className='col-md-8'>
+                <div className='col-md-6'>
                   <Jumbotron className='about-me'>
                       <p>
                       Well, first of all, a big sports addict. Be it cricket, football or badminton, you can count me in as a  participant or as a spectator. Also loves comic book stuffs, be it DC or Marvel. Ready to join in any fanboy discussions! 
