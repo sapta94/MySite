64bf5b82f23b49c787aa7a86b1c54bc05d2337cc
sapta94 <dey7.kol@gmail.com>
2020-01-11 12:47:13 +0530

    showing details in each card for expertise

--- src/app/components/Expertise.js
+++ src/app/components/Expertise.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Accordion from 'react-bootstrap/Accordion'
-import {Card} from "react-bootstrap"
+import {Card,ProgressBar} from "react-bootstrap"
 const expertData = {
     labels: [
       'Back-End',
@@ -60,26 +60,75 @@ export default class Expertise extends Component {
         <Accordion defaultActiveKey="0">
           <Card>
             <Accordion.Toggle as={Card.Header} eventKey="0">
-              Backend
+              Backend Technologies
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
-              <Card.Body>Hello! I'm the body</Card.Body>
+              <Card.Body>
+              <div>
+                <label className='expertise-tags'>Node JS</label>
+                <ProgressBar label='80%' variant="success" now={80} />
+                <br/>
+                <label className='expertise-tags'>PHP</label>
+                <ProgressBar label='70%' variant="success" now={70} />
+                <br/>
+                <label className='expertise-tags'>Python</label>
+                <ProgressBar label='50%'  variant="success" now={50} />
+            </div>
+              </Card.Body>
             </Accordion.Collapse>
           </Card>
           <Card>
             <Accordion.Toggle as={Card.Header} eventKey="1">
-              Frontend
+              Databases
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="1">
-              <Card.Body>Hello! I'm another body</Card.Body>
+              <Card.Body>
+                <div>
+                  <label className='expertise-tags'>RDBMS(MySQL/Postgres)</label>
+                  <ProgressBar label='80%' variant="info" now={80} />
+                  <br/>
+                  <label className='expertise-tags'>NoSQL(MongoDB)</label>
+                  <ProgressBar label='60%' variant="info" now={60} />
+                </div>
+              </Card.Body>
             </Accordion.Collapse>
           </Card>
           <Card>
             <Accordion.Toggle as={Card.Header} eventKey="2">
-              DevOps
+              Frontend
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="2">
-              <Card.Body>Hello! I'm another body</Card.Body>
+              <Card.Body>
+                <div>
+                  <label className='expertise-tags'>HTML/CSS</label>
+                  <ProgressBar label='80%' variant="success" now={80} />
+                  <br/>
+                  <label className='expertise-tags'>JQuery</label>
+                  <ProgressBar label='70%' variant="success" now={70} />
+                  <br/>
+                  <label className='expertise-tags'>ReactJS</label>
+                  <ProgressBar label='70%' variant="success" now={70} />
+                </div>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="3">
+              DevOps
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="3">
+              <Card.Body>
+                <div>
+                  <label className='expertise-tags'>Google Cloud</label>
+                  <ProgressBar label='70%' variant="info" now={70} />
+                  <br/>
+                  <label className='expertise-tags'>AWS</label>
+                  <ProgressBar label='60%' variant="info" now={60} />
+                  <br/>
+                  <label className='expertise-tags'>Docker/Kubernetes</label>
+                  <ProgressBar label='50%' variant="info" now={50} />
+                </div>
+              </Card.Body>  
             </Accordion.Collapse>
           </Card>
         </Accordion>
