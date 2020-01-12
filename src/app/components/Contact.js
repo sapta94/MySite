458a99a3cc9007ac44004969f50913f4074bdd12
sapta94 <dey7.kol@gmail.com>
2020-01-12 23:08:35 +0530

    contact icons added

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -14,7 +14,27 @@ export default class Contact extends Component {
         <div className='section-headers' >
             -Contact-
         </div>
-        
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='contact-icons col-md-8 offset-md-3'>
+              <div className='row'>
+                <div className='col-md-4 col-sm-4'>
+                  <i className="fa fa-envelope fa-2x"></i>
+                  <p>dey7.kol@gmail.com</p>
+                </div>
+                <div className='col-md-4 col-sm-4'>
+                  <i className="fa fa-map-marker fa-2x"></i>
+                  <p>Bengaluru, India</p>
+                </div>
+                <div className='col-md-4 col-sm-4'>
+                  <i className="fa fa-phone fa-2x"></i>
+                  <p>+8981235949</p>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>  
        </div>
     );
   }
