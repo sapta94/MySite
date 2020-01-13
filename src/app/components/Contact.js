2ed721e11335b38c7894462158b684d10c319249
sapta94 <dey7.kol@gmail.com>
2020-01-13 22:55:26 +0530

    contact form added

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Form,Button} from "react-bootstrap"
 
 
 export default class Contact extends Component {
@@ -16,7 +17,7 @@ export default class Contact extends Component {
         </div>
         <div className='container-fluid'>
           <div className='row'>
-            <div className='contact-icons col-md-8 offset-md-3'>
+            <div className='contact-icons col-md-8 offset-md-3 '>
               <div className='row'>
                 <div className='col-md-4 col-sm-4'>
                   <i className="fa fa-envelope fa-2x"></i>
@@ -28,12 +29,32 @@ export default class Contact extends Component {
                 </div>
                 <div className='col-md-4 col-sm-4'>
                   <i className="fa fa-phone fa-2x"></i>
-                  <p>+8981235949</p>
+                  <p>+91-8981235949</p>
                 </div>
               </div>
               
             </div>
           </div>
+          <div className='row'>
+            <div className='col-md-8 offset-md-2' style={{marginTop:'2%',marginBottom:'2%'}}>
+              <div className='row'>
+                <div className='col-md-6'>
+                  
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </div>
+                <div className='col-md-6'>
+                  <Form.Control type="number" placeholder="Mobile No. here" />
+                </div>
+              </div>
+            </div>
+            
+            <div className='col-md-8 offset-md-2'>
+                <Form.Label>Your message here!</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+            </div>
+            
+          </div>
+          <center><Button style={{marginTop:'2%'}} className='custom-button' size='lg' target='_blank'>Send the message :)</Button></center>
         </div>  
        </div>
     );
