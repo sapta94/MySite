3e78b7c5340e34e39a11c77c7b345cd72995d92b
sapta94 <dey7.kol@gmail.com>
2020-01-14 21:48:34 +0530

    added sms sending

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import {Form,Button} from "react-bootstrap"
+import axios from "axios"
 
 
 export default class Contact extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      email:null,
+      mobile:null,
+      message:null
+    }
+  }
+
+  handleChange=(id,e)=>{
+    if(id==1){
+      this.setState({
+        email:e.target.value
+      })
+    }
+    if(id==2){
+      this.setState({
+        mobile:e.target.value
+      })
+    }
+    if(id==3){
+      this.setState({
+        message:e.target.value
+      })
+    }
+  }
 
   handleClick=(id)=>{
     let url="";
     url=(id==1)?'github.com/sapta94':(id==2)?'angel.co/saptarshi-dey-1995':(id==3)?'quora.com/profile/Saptarshi-Dey-22':'facebook.com/saptarshi.cena'
     window.open('https://'+url)
+  }
+
+  sendMessage=()=>{
+    axios.post('https://api.textlocal.in/send/?',{
+      'apikey': '8inchm9Kjig-DjYCrIxZhI4yrTBTyw3tOF0dB00VOx', 'numbers': 8981235949,
+      'message' : this.state.message, 'sender': this.state.mobile
+    }).then((resp)=>{
+      console.log(resp)
+    }).catch(err=>{
+      console.log(err)
+    })
   }
   render() {
     return (
@@ -38,23 +76,23 @@ export default class Contact extends Component {
           <div className='row'>
             <div className='col-md-8 offset-md-2' style={{marginTop:'2%',marginBottom:'2%'}}>
               <div className='row'>
-                <div className='col-md-6'>
+                <div className='col-md-6 mob-view'>
                   
-                  <Form.Control type="email" placeholder="name@example.com" />
+                  <Form.Control type="email" value={this.state.email} placeholder="name@example.com" onChange={(e)=>this.handleChange(1,e)}/>
                 </div>
-                <div className='col-md-6'>
-                  <Form.Control type="number" placeholder="Mobile No. here" />
+                <div className='col-md-6 mob-view'>
+                  <Form.Control type="number" value={this.state.mobile} placeholder="Mobile No. here" onChange={(e)=>this.handleChange(2,e)}/>
                 </div>
               </div>
             </div>
             
             <div className='col-md-8 offset-md-2'>
                 <Form.Label>Your message here!</Form.Label>
-                <Form.Control as="textarea" rows="3" />
+                <Form.Control as="textarea" rows="3" value={this.state.message} onChange={(e)=>this.handleChange(3,e)}/>
             </div>
             
           </div>
-          <center><Button style={{marginTop:'2%'}} className='custom-button' size='lg' target='_blank'>Send the message :)</Button></center>
+          <center><Button onClick={()=>this.sendMessage()} style={{marginTop:'2%'}} className='custom-button' size='lg' target='_blank'>Send the message :)</Button></center>
         </div>  
        </div>
     );
